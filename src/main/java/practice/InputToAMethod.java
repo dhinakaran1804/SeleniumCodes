55662a0ee83a6a1f89f2bc86646fee1e18eec58c
dhinakaran1804 <dhinakarankvp@gmail.com>
2023-04-26 16:05:52 +0530

    Code pushed

---
+++ src/main/java/practice/InputToAMethod.java
@@ -0,0 +1,27 @@
+package practice;
+public class InputToAMethod {
+
+	public void sum(int num1, int num2, float num3) { // --> Parameterized method
+
+		System.out.println(num1 + num2 - num3);
+		System.out.println(num1-num2);
+		System.out.println("Text in sub method");
+	}
+
+	private void getEmployeeInfo(String firstName, String lastName) {
+		System.out.println(firstName + " " + lastName);
+	}
+
+	private void intAndString(int a, int b, String adding) {
+		System.out.println(a + b + " " + adding);
+	}
+
+	public static void main(String[] args) {
+		InputToAMethod liao = new InputToAMethod();
+		
+		liao.sum(50, 70, 10.50f); // --> Argument
+		liao.getEmployeeInfo("Dhinakaran", "S");
+		liao.intAndString(10, 15, "Added");
+		System.out.println("Text in main method");
+	}
+}
---
+++ src/main/java/practice/OutputFromAMethod.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class OutputFromAMethod {
+
+	public int add(int a, int b) {
+		int addition = a + b;
+		System.out.println(addition);
+		return addition; // --> Should be end of the method & Only one.
+	}
+
+	public double sub(int a, int b, double c) {
+		double subraction = a + b - c;
+		System.out.println(subraction);
+		return subraction;
+	}
+
+	public static void main(String[] args) {
+		OutputFromAMethod value = new OutputFromAMethod();
+
+		int addition1 = value.add(10, 10);
+		System.out.println(addition1 + 10);
+
+		double subraction1 = value.sub(10, 220, 75.80);
+		System.out.println(subraction1 - 10);
+
+	}
+
+}
