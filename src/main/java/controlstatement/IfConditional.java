18d5bc8235ce26c086643e47ba064120fd139cde
dhinakaran1804 <dhinakarankvp@gmail.com>
2023-04-30 10:45:11 +0530

    If statement update

--- src/main/java/controlstatement/IfConditional.java
+++ src/main/java/controlstatement/IfConditional.java
@@ -4,25 +4,27 @@ public class IfConditional {
 
 	public void oneif(int a) {
 
-		if (a > 10) {
+		if (a > 10)
 			System.out.println("A value is more then 10");
-		}
-
 	}
 
 	public void twoif(int a) {
 
-		if (a > 10) {
+		if (a > 10)
 			System.out.println("A value is more then 10");
-		}
 		System.out.println("A value is less then 10");
-
 	}
 
 	public static void main(String[] args) {
 		IfConditional iff = new IfConditional();
 		iff.oneif(15);
 		iff.twoif(8);
+
+		int a = 10;
+		if (a > 15)
+			System.out.println("A value is grater then 10");
+		System.out.println("A value is less then 10");
+
 	}
 
 }
